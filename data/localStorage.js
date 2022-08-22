88c9ee7ad9c7a64346f3783e6a9d9434e233ae6f
Sanni Oluwafikayo <fikayo.sanni@gmail.com>
2022-08-22 14:22:39 +0100

    Update localStorage.js

--- data/localStorage.js
+++ data/localStorage.js
@@ -1,40 +1,44 @@
-let storeData, updateData, retrieveData, removeData;
-if (typeof window !== "undefined") {
-  storeData = async (label, data) => {
-    try {
-      let response = await localStorage.setItem(label, data.toString());
+export const storeData = async( label,data ) =>{
+  try{
+    if (typeof window !== "undefined") {
+      let response = await localStorage.setItem(label,data.toString())
       return Promise.resolve(response);
-    } catch (e) {
-      return Promise.reject(e);
     }
-  };
-
-  updateData = async (label, data) => {
-    try {
-      let response = await localStorage.mergeItem(label, data.toString());
-      return Promise.resolve(response);
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
-
-  retrieveData = async (label) => {
-    try {
-      let response = await localStorage.getItem(label);
-      return Promise.resolve(response);
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
-
-  removeData = async (label) => {
-    try {
-      let response = await localStorage.removeItem(label);
-      return Promise.resolve(response);
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  };
+  }catch(e){
+    return Promise.reject(e);
+  }
 }
 
-export {storeData, updateData, retrieveData, removeData}
+
+export const updateData = async(label,data) =>{
+  try{
+    if (typeof window !== "undefined") {
+      let response = await localStorage.mergeItem(label,data.toString())
+      return Promise.resolve(response);
+    }
+  }catch(e){
+    return Promise.reject(e);
+  }
+}
+
+export const retrieveData = async(label)=>{
+  try{
+    if (typeof window !== "undefined") {
+      let response = await localStorage.getItem(label);
+      return Promise.resolve(response);
+    }
+  }catch(e){
+    return Promise.reject(e);
+  }
+}
+
+export const removeData = async(label)=>{
+  try{
+    if (typeof window !== "undefined") {
+      let response = await localStorage.removeItem(label);
+      return Promise.resolve(response)
+    }
+  }catch(e){
+    return Promise.reject(e);
+  }
+}
