61bcd7604c1d0217b45f605d0fc4fbee224da971
Sanni Oluwafikayo <fikayo.sanni@gmail.com>
2022-09-06 06:04:54 +0100

    chore: fix build errors

--- pages/partners/index.js
+++ pages/partners/index.js
@@ -37,7 +37,7 @@ const SubMenu = Menu.SubMenu;
 const TabPane = Tabs;
 
 const Index = (props) => {
-  const config = useSelector((state) => state.app);
+  // const config = useSelector((state) => state.app);
 
   useEffect(async () => {}, []);
 
