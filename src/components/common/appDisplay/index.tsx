0dd0cfcd7b6ff91da7ec31a1a0314d25d0932418
Eddy G.U <eddygudom@gmail.com>
2023-01-08 13:54:14 +0100

    open app

--- src/components/common/appDisplay/index.tsx
+++ src/components/common/appDisplay/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { Avatar, Card, Tag, Typography } from 'antd';
 import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+import { setCurrentApp } from '../../../redux/applicationSlice';
+import Router from 'next/router';
 
 const { Title, Text, Paragraph } = Typography;
 
@@ -10,35 +14,42 @@ interface Props {
 }
 
 const AppDisplay: React.FC<Props> = ({ app, orientation = 'grid' }) => {
+    const dispatch = useDispatch();
+
+    const { user, defaultWorkspaceId } = useSelector((state: RootState) => state.app);
+
+    const openApp = async (app) => {
+        await dispatch(setCurrentApp(app));
+        Router.push(`/apps/${app._id}`);
+    };
+
     return orientation === 'grid' ? (
-        <Link href={`/apps/${app._id}`}>
-            <div className="col-xl-4">
-                <Card
-                    className=" mb-4"
-                    actions={[
-                        <Text>{app.domains ? app.domains.length : 0} domains</Text>,
-                        <Text>{app.envs.length} environments</Text>,
-                    ]}
-                >
-                    <div className="d-flex justify-content-between align-items-start">
-                        <div className="d-flex flex-row gap-2">
-                            <Avatar className="font-sm-2" shape="square" size="large">
-                                {app.app_name.charAt(0)}
-                            </Avatar>
-                            <div>
-                                <Title className="mb-0" level={5}>
-                                    {app.app_name}
-                                </Title>
-                                <Text type="secondary" className="text-capitalize mb-0">
-                                    {app.status}
-                                </Text>
-                            </div>
+        <div className="col-xl-4 cursor_pointer" onClick={() => openApp(app)}>
+            <Card
+                className=" mb-4"
+                actions={[
+                    <Text>{app.domains ? app.domains.length : 0} domains</Text>,
+                    <Text>{app.envs.length} environments</Text>,
+                ]}
+            >
+                <div className="d-flex justify-content-between align-items-start">
+                    <div className="d-flex flex-row gap-2">
+                        <Avatar className="font-sm-2" shape="square" size="large">
+                            {app.app_name.charAt(0)}
+                        </Avatar>
+                        <div>
+                            <Title className="mb-0" level={5}>
+                                {app.app_name}
+                            </Title>
+                            <Text type="secondary" className="text-capitalize mb-0">
+                                {app.status}
+                            </Text>
                         </div>
-                        {app.active ? <Tag color="green">Active</Tag> : <Tag color="red">Inactive</Tag>}
                     </div>
-                </Card>
-            </div>
-        </Link>
+                    {app.active ? <Tag color="green">Active</Tag> : <Tag color="red">Inactive</Tag>}
+                </div>
+            </Card>
+        </div>
     ) : (
         <div className="col-xl-12">
             <Card className="mb-4">
--- src/pages/apps/[id]/index.js
+++ src/pages/apps/[id]/index.js
@@ -1,34 +1,29 @@
-import App_Layout from "../../../components/layout/app_layout";
-import Overview from "../../../components/apps/app/overview";
-import toast from "react-hot-toast";
-import { useState } from "react";
+import Dashboard_Layout from '../../../components/layout/dashboard_layout';
+import Overview from '../../../components/apps/app/overview';
+import { setCurrentApp } from '../../../redux/applicationSlice';
+import { useState } from 'react';
 
 const Index = (props) => {
-  const { app_id } = props;
+    const { app_id } = props;
 
-  //alert(JSON.stringify(props));
+    //alert(JSON.stringify(props));
 
-  const [text, setText] = useState("");
-  const [html, setHTML] = useState("");
+    const [text, setText] = useState('');
+    const [html, setHTML] = useState('');
 
-  return (
-    <App_Layout
-      app_id={app_id}
-      selected="1"
-      setHTML={setHTML}
-      setText={setText}
-    >
-      <Overview app_id={app_id} defaultText={text} defaultHTML={html}/>
-    </App_Layout>
-  );
+    return (
+        <Dashboard_Layout showSidebar={true} title="Apps">
+            <Overview app_id={app_id} defaultText={text} defaultHTML={html} />
+        </Dashboard_Layout>
+    );
 };
 
 export default Index;
 
 export const getServerSideProps = async ({ params }) => {
-  const app_id = params.id;
+    const app_id = params.id;
 
-  return {
-    props: { app_id },
-  };
+    return {
+        props: { app_id },
+    };
 };
