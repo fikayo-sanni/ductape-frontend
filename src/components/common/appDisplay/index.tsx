27bf0f3e6e19fb2f1f786a731af5d2311346aa2d
Eddy G.U <eddygudom@gmail.com>
2023-01-08 13:43:45 +0100

    update apps

--- src/components/common/appDisplay/index.tsx
+++ src/components/common/appDisplay/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Avatar, Card, Tag, Typography } from 'antd';
+import Link from 'next/link';
 
 const { Title, Text, Paragraph } = Typography;
 
@@ -10,32 +11,34 @@ interface Props {
 
 const AppDisplay: React.FC<Props> = ({ app, orientation = 'grid' }) => {
     return orientation === 'grid' ? (
-        <div className="col-xl-4">
-            <Card
-                className=" mb-4"
-                actions={[
-                    <Text>{app.domains ? app.domains.length : 0} domains</Text>,
-                    <Text>{app.envs.length} environments</Text>,
-                ]}
-            >
-                <div className="d-flex justify-content-between align-items-start">
-                    <div className="d-flex flex-row gap-2">
-                        <Avatar className="font-sm-2" shape="square" size="large">
-                            {app.app_name.charAt(0)}
-                        </Avatar>
-                        <div>
-                            <Title className="mb-0" level={5}>
-                                {app.app_name}
-                            </Title>
-                            <Text type="secondary" className="text-capitalize mb-0">
-                                {app.status}
-                            </Text>
+        <Link href={`/apps/${app._id}`}>
+            <div className="col-xl-4">
+                <Card
+                    className=" mb-4"
+                    actions={[
+                        <Text>{app.domains ? app.domains.length : 0} domains</Text>,
+                        <Text>{app.envs.length} environments</Text>,
+                    ]}
+                >
+                    <div className="d-flex justify-content-between align-items-start">
+                        <div className="d-flex flex-row gap-2">
+                            <Avatar className="font-sm-2" shape="square" size="large">
+                                {app.app_name.charAt(0)}
+                            </Avatar>
+                            <div>
+                                <Title className="mb-0" level={5}>
+                                    {app.app_name}
+                                </Title>
+                                <Text type="secondary" className="text-capitalize mb-0">
+                                    {app.status}
+                                </Text>
+                            </div>
                         </div>
+                        {app.active ? <Tag color="green">Active</Tag> : <Tag color="red">Inactive</Tag>}
                     </div>
-                    {app.active ? <Tag color="green">Active</Tag> : <Tag color="red">Inactive</Tag>}
-                </div>
-            </Card>
-        </div>
+                </Card>
+            </div>
+        </Link>
     ) : (
         <div className="col-xl-12">
             <Card className="mb-4">
