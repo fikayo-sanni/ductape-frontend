104c7d07232cc0d619a27022a218be13cdabd914
Eddy G.U <eddygudom@gmail.com>
2023-01-28 09:55:30 +0100

    update

--- src/pages/apps/[id]/environments.tsx
+++ src/pages/apps/[id]/environments.tsx
@@ -2,31 +2,30 @@ import React, { useState } from 'react';
 import Dashboard_Layout from '../../../components/layout/dashboard_layout';
 import PageHeader from '../../../components/common/pageHeader';
 import dynamic from 'next/dynamic';
-import { Button, Card, Modal, Typography } from 'antd';
+import { Button, Modal, Typography } from 'antd';
 import { PlusCircleOutlined } from '@ant-design/icons';
 
+const { Title, Text } = Typography;
 const AppEnvironments = dynamic(() => import('../../../components/app/environments'));
 
 const Environments = () => {
-    const [visible, setVisible] = useState(true);
+    const [visible, setVisible] = useState(false);
+
+    const handleClick = () => {
+        setVisible(!visible);
+    };
 
     return (
         <Dashboard_Layout showSidebar={true} title="Apps" appPage="Environments">
-            <Card className="rounded-0 border-top-0 border-start-0 border-end-0 py-2">
-                <div className="container d-flex flex-row justify-content-between align-items-center">
-                    <Typography.Title className="mb-0" level={2}>
-                        Environments
-                    </Typography.Title>
-
-                    <div className="title_extra">
-                        dd
-                        <Button onClick={() => setVisible(true)}>
-                            <PlusCircleOutlined /> Create Environment
-                        </Button>
-                    </div>
-                </div>
-            </Card>
-
+            <PageHeader
+                title="Environments"
+                handleClick={handleClick}
+                extra={
+                    <>
+                        <PlusCircleOutlined /> Create Environment
+                    </>
+                }
+            />
             <AppEnvironments />
 
             <Modal
@@ -38,7 +37,6 @@ const Environments = () => {
                         <Text type="secondary text-uppercase">Environment</Text>
                     </div>
                 }
-                width={1000}
                 open={visible}
                 footer={null}
                 onCancel={() => setVisible(false)}
